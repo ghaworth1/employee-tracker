bd91baa5b56a17a3783f67b93bf512952b0060da
George Haworth <ghaworth1@gmail.com>
2023-08-30 00:03:56 -0500

    seeding

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+-- INSERT INTO department (name)
+-- VALUES ("Web Development"),
+--        ("Data Science"),
+--        ("Math"),
+--        ("Electives");
+-- INSERT INTO course_names (name, department)
+-- VALUES ("Intro to JavaScript", 1),
+--        ("Data Science", 2),
+--        ("Linear Algebra", 3),
+--        ("History of the Internet", 4),
+--        ("Machine Learning", 4),
+--        ("Game Design", 1 ),
+--        ("Cloud Development", 1);
+       
+
+
+INSERT INTO department (name)
+VALUES ("R and D"),
+       ("Sales"),
+       ("Support"),
+       ("Administration");
+
+INSERT INTO role (name, department_id)
+       ("Engineering", 1),
+
+INSERT INTO employee (id, name, department_id, manager_id)
+       (001, "Macareno Haworth", 2, )
